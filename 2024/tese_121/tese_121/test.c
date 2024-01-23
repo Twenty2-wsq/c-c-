44a5c349dcc7e3fc25210b25036800815cb11b5f
Twenty2-wsq <111562538+Twenty2-wsq@users.noreply.github.com>
2024-01-23 22:39:28 +0800

    Add files via upload

---
+++ 2024/tese_121/tese_121/test.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//标准的主函数写法
+// ctrl+k+c 注释 ctrl+k+u 取消注释 
+/*
+int main()
+{
+	printf("Hello Word!!!\n");
+
+	return 0;
+}
+*/
+
+// 1 byte = 8 bit = 1024 kb
+//int main()
+//{
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	printf("%zu\n", sizeof(float));
+//	printf("%zu\n", sizeof(double));
+//
+//	return 0;
+//}
+
+//变量
+//局部变量 ang 全局变量
+
+//int a = 66; //全局变量
+//
+//int main()
+//{
+//	short age = 23;
+//	int high = 176;
+//	float weight = 66;
+//
+//	int b = 66; //局部变量 局部优先
+//
+//	return 0;
+//}
+
+//计算二个整数的和
+//int main()
+//{
+//	//初始化
+//	int num1 = 0;
+//	int num2 = 0;
+//	//输入两个值
+//	scanf("%d %d", &num1, &num2);
+//	//求和
+//	int sum = num1 + num2;
+//	//输出
+//	printf("num1 + num = %d\n",sum ); 
+//
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
